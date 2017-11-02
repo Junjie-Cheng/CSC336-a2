bcac0165be588e5333265c962ca50df9735579be
Kai <junjie.cheng@mail.utoronto.ca>
2017-11-02 18:33:23 -0400

    Q4

---
+++ Question4a.m
@@ -0,0 +1,48 @@
+function Question4a(~)
+
+Alpha = sqrt(2)/2;
+A = zeros(13, 13);
+
+A(1,2) = 1;
+A(1,6) = -1;
+A(2,3) = 1;
+A(3,1) = Alpha;
+A(3,4) = -1;
+A(3,5) = -Alpha;
+A(4,1) = Alpha;
+A(4,3) = 1;
+A(4,5) = Alpha;
+A(5,4) = 1;
+A(5,8) = -1;
+A(6,7) = 1;
+A(7,5) = Alpha;
+A(7,6) = 1;
+A(7,9) = -Alpha;
+A(7,10) = -1;
+A(8,5) = Alpha;
+A(8,7) = 1;
+A(8,9) = Alpha;
+A(9,10) = 1;
+A(9,13) = -1;
+A(10,11) = 1;
+A(11,8) = 1;
+A(11,9) = Alpha;
+A(11,12) = -Alpha;
+A(12,9) = Alpha;
+A(12,11) = 1;
+A(12,12) = Alpha;
+A(13,13) = 1;
+A(13,12) = Alpha;
+
+b = zeros(13,1);
+b(2) = 10;
+b(8) = 15;
+b(10) = 20;
+
+f = A\b
+
+condA = cond(A, 2);
+
+fprintf('Conditional Number of A is %f\n', condA);
+
+end
